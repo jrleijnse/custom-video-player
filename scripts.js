6108ef4e0f182abfe3f9063f305f30b30d870a6b
Jeroen Leijnse <jr_leijnse@live.nl>
2023-02-26 12:06:26 +0100

    Initial commit

---
+++ scripts.js
@@ -0,0 +1,49 @@
+const videoScreen = document.querySelector(".viewer");
+const media = document.querySelector("video");
+
+const playBtn = document.querySelector(".player__button.toggle");
+const rewindBtn = document.querySelector('.player__button[data-skip="-10"]');
+const forwardBtn = document.querySelector('.player__button[data-skip="25"]');
+
+const volume = document.querySelector('.player__slider[name="volume"]');
+const playbackRate = document.querySelector(
+  '.player__slider[name="playbackRate"]'
+);
+
+const updateButton = function () {
+  media.paused ? (playBtn.textContent = "❚ ❚") : (playBtn.textContent = "►");
+};
+
+const playVideo = function () {
+  if (media.paused) {
+    media.play();
+  } else {
+    media.pause();
+  }
+};
+
+const forward = function () {
+  media.currentTime += +forwardBtn.getAttribute("data-skip", "25");
+  console.log(media.currentTime);
+};
+
+const rewind = function () {
+  media.currentTime += +rewindBtn.getAttribute("data-skip", "-10");
+  console.log(media.currentTime);
+};
+
+volume.addEventListener("input", function (e) {
+  media.volume = e.currentTarget.value;
+});
+playbackRate.addEventListener("input", function (e) {
+  media.playbackRate = e.currentTarget.value;
+});
+
+// rewindBtn.addEventListener("click", rewind);
+forwardBtn.addEventListener("click", forward);
+rewindBtn.addEventListener("click", rewind);
+playBtn.addEventListener("click", playVideo);
+videoScreen.addEventListener("click", playVideo);
+videoScreen.addEventListener("keyup", playVideo);
+videoScreen.addEventListener("play", updateButton);
+videoScreen.addEventListener("pause", updateButton);
