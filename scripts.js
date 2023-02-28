88653cce4bc6900264a9566636bce5ed263c27ca
Jeroen Leijnse <jr_leijnse@live.nl>
2023-02-28 21:42:10 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,16 +1,14 @@
 const videoScreen = document.querySelector(".viewer");
-
 const progress = document.querySelector(".progress");
-
 const progressBar = document.querySelector(".progress__filled");
 const media = document.querySelector("video");
 const playBtn = document.querySelector(".player__button.toggle");
-const rewindBtn = document.querySelector('.player__button[data-skip="-10"]');
-const forwardBtn = document.querySelector('.player__button[data-skip="25"]');
-const volume = document.querySelector('.player__slider[name="volume"]');
-const playbackRate = document.querySelector(
-  '.player__slider[name="playbackRate"]'
-);
+const skipButtons = document.querySelectorAll(".player__button[data-skip]");
+// const volume = document.querySelector('.player__slider[name="volume"]');
+// const playbackRate = document.querySelector(
+//   '.player__slider[name="playbackRate"]'
+// );
+const ranges = document.querySelectorAll("[type=range]");
 
 const playVideo = function () {
   if (media.paused) {
@@ -34,42 +32,62 @@ const updateButton = function () {
   this.paused ? (playBtn.textContent = "►") : (playBtn.textContent = "❚ ❚");
 };
 
-const forward = function () {
-  media.currentTime += +this.getAttribute("data-skip", "25");
+const skip = function () {
+  if (this.dataset.skip === "25") {
+    media.currentTime += +this.dataset.skip;
+  }
+  if (this.dataset.skip === "-10") {
+    media.currentTime += +this.dataset.skip;
+  }
 };
 
-const rewind = function () {
-  media.currentTime += +this.getAttribute("data-skip", "-10");
+function handleRangeUpdate() {
+  media[this.name] = this.value;
+}
+
+const handleProgress = function () {
+  progressBar.style.flexBasis = `${(100 / this.duration) * this.currentTime}%`;
+};
+
+const selectTimestamp = function (e) {
+  const timeStamp = (e.offsetX / progress.offsetWidth) * media.duration;
+  media.currentTime = timeStamp;
 };
 
 const arrowDuration = function (e) {
   if (e.key === "ArrowLeft") {
-    media.currentTime += +rewindBtn.getAttribute("data-skip", "-10");
+    media.currentTime += -10;
   }
 
   if (e.key === "ArrowRight") {
-    media.currentTime += +forwardBtn.getAttribute("data-skip", "25");
+    media.currentTime += 25;
   }
 };
 
-const handleProgress = function () {
-  progressBar.style.flexBasis = `${(100 / media.duration) * this.currentTime}%`;
-};
-
-volume.addEventListener("input", function (e) {
-  media.volume = e.currentTarget.value;
-});
-playbackRate.addEventListener("input", function (e) {
-  media.playbackRate = e.currentTarget.value;
-});
-
 window.addEventListener("keyup", playVideoSpace);
 window.addEventListener("keydown", arrowDuration);
+
 videoScreen.addEventListener("click", playVideo);
-playBtn.addEventListener("click", playVideo);
 videoScreen.addEventListener("play", updateButton);
 videoScreen.addEventListener("pause", updateButton);
-
-forwardBtn.addEventListener("click", forward);
-rewindBtn.addEventListener("click", rewind);
 videoScreen.addEventListener("timeupdate", handleProgress);
+
+playBtn.addEventListener("click", playVideo);
+skipButtons.forEach((button) => button.addEventListener("click", skip));
+
+// volume.addEventListener("input", function (e) {
+//   media.volume = e.currentTarget.value;
+// });
+// playbackRate.addEventListener("input", function (e) {
+//   media.playbackRate = e.currentTarget.value;
+// });
+ranges.forEach((range) => range.addEventListener("change", handleRangeUpdate));
+ranges.forEach((range) =>
+  range.addEventListener("mousemove", handleRangeUpdate)
+);
+
+let mousedown = false;
+progress.addEventListener("click", selectTimestamp);
+progress.addEventListener("mousemove", (e) => mousedown && selectTimestamp(e));
+progress.addEventListener("mousedown", () => (mousedown = true));
+progress.addEventListener("mouseup", () => (mousedown = false));
